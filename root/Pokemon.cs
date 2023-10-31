75b98b8fd6f57711fac726f0448841d4a567612b
MagneTree <magnes05@outlook.dk>
2023-10-31 09:47:50 +0100

    pokemon stats

---
+++ root/Pokemon.cs
@@ -0,0 +1,20 @@
+namespace root;
+
+public class Pokemon
+{
+    private int hp;
+    private int attack;
+    private int defense;
+    private int speed;
+    private int accuracy;
+
+
+    public Pokemon(int hp, int attack, int defense, int speed, int accuracy)
+    {
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.accuracy = accuracy;
+    }
+}
